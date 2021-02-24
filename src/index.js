1f01f5ddb107da39106a7fe08f4e98a2b6b372d5
Pototex <gustavo.matarrita@nacion.com>
2021-02-24 08:00:00 -0600

    Clase JSX

--- src/index.js
+++ src/index.js
@@ -8,9 +8,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
 
+
+//const element = <h1>Hello, Platzi Badges!</h1>;
+//const element = React.createElement('a', {href: 'https://platzi.com'}, 'Ir a Platzi');
+
+const name = 'Richard'
+//const element = React.createElement('h1', {}, `Hola, soy ${name}`)
+const sum = () => 3 + 3
+const jsx = <h1>Hola, soy {sum()}</h1>
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
+
